c70094d92189934fa64db47e780bbeab3037b979
anshid <anshidanu99@gmail.com>
2024-03-15 11:09:10 +0530

    first commit

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,32 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns a 201 on successful signup", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("sets a cookie after successfull signup", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
---
+++ client/pages/auth/signup.js
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <form>
+      <h1>signup form</h1>
+    </form>
+  );
+};
